58f57945617505e3d98667a74ff793495c829749
fifanuurunhalizah09 <eljeaje@gmail.com>
2024-10-03 07:56:55 +0700

    modifikasiPercobaan1

--- Pemilihan2Percobaan109.java
+++ Pemilihan2Percobaan109.java
@@ -13,8 +13,11 @@ public class Pemilihan2Percobaan109 {
     if ((tahun % 4) == 0) {
         if ((tahun % 100) != 0)
             System.out.println("Tahun Kabisat");
+        else{
+            System.out.println("Bukan Tahun Kabisat");
+        }
     } else 
-        System.out.println("bBukan Tahun Kabisat");
+        System.out.println("Bukan Tahun Kabisat");
     
     }
 }
